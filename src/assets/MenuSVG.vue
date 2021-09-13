a4314706dade270e433d1ce99bf189270cb84b37
daklo91 <coffeedog91@gmail.com>
2021-09-13 16:39:12 +0200

    Add MenuSVG

---
+++ src/assets/MenuSVG.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    width="24"
+    height="17"
+    viewBox="0 0 24 17"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="3" />
+    <rect y="7" width="24" height="3" />
+    <rect y="14" width="24" height="3" />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+rect {
+  fill: $primary-color;
+}
+</style>
