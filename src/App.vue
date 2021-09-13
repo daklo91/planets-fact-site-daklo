e8380047bb8b6acbfa66b8ebc5a164d3b25139c6
daklo91 <coffeedog91@gmail.com>
2021-09-13 14:47:03 +0200

    Add scss variables from figma

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,23 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Antonio&family=Spartan&display=swap");
+
+$background-color: #070724;
+$primary-color: #fff;
+$secondary-color: #838391;
+$hover-color: #38384f;
+
+$headline-font-family: "Antonio", sans-serif;
+$text-font-family: "Spartan", sans-serif;
+
+* {
+  background-color: $background-color;
+  color: $primary-color;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $headline-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
