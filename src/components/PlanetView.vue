be88c4f944e3008592fdefbf0dfd3454b3f6a09f
daklo91 <coffeedog91@gmail.com>
2021-09-13 16:39:39 +0200

    Add PlanetView

---
+++ src/components/PlanetView.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="">OVERVIEW</a></li>
+      <li><a href="">STRUCTURE</a></li>
+      <li><a href="">SURFACE</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
